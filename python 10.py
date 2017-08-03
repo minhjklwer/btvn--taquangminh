879ce70cbc09821f823ca7f068bc3ef4212b8522
ADMINISTRATOR\Administrator <quangminhta5@gmail.com>
2017-08-03 10:54:18 +0700

    homework commit

---
+++ python 10.py
@@ -0,0 +1,21 @@
+print("ThiS is String with Upper and lower case Letters")
+
+String ={
+    "a" : 2,
+    "c" :1,
+    "d" :1,
+    "e" : 5,
+    "g" : 2,
+    "h" : 2,
+    "i" :4,
+    "l" :2,
+    "n" :2,
+    "o" : 1,
+    "p" :2,
+    "r" :4,
+    "s" :5,
+    "t" :5,
+    "u" :1,
+    "w" : 2
+    }
+print(String)
---
+++ python 12.py
@@ -0,0 +1,11 @@
+prices = {}
+prices["banana"] = 4
+prices["apple"] = 2
+prices["orange"] = 1.5
+prices["pear"] = 3
+print(prices)
+
+purchased_items = {
+    "banana": 5,
+    "orange": 3
+    }
