a29a841ab791caa33a7deae8c968f9aa2039a90a
ADMINISTRATOR\Administrator <quangminhta5@gmail.com>
2017-08-13 22:46:20 +0700

    BTVN sesion 7

---
+++ btvnsession7/btvnsession7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace btvnsession7
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{ 
+			while(true){
+				
+			Random rnd = new Random ();
+			int nums = rnd.Next(1 ,100);
+
+
+			
+			int convertnumber;
+			int number;
+
+				while (true) 
+				{
+					convertnumber = (nums % 2);
+
+					number = (nums / 2);
+
+					Console.Write (convertnumber);
+
+					nums = number;
+
+					if (number == 0) 
+					{
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
